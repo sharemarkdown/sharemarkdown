e8a68c78461ad17f9019cf5f127f9492581d8ff2
Paul Luh <paul841029@gmail.com>
2018-04-22 22:02:01 -0500

    add docs api

--- project/sharemarkdown/urls.py
+++ project/sharemarkdown/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 import rest_framework.authtoken.views as token_view
 urlpatterns = [
-    path('api/user/', views.ListCreateUser.as_view()),
-    path('api/document/', views.ListCreateDocument.as_view()),
-    path('api/folder/', views.ListCreateFolder.as_view()),
-    path('api/register/', views.CreateUserView.as_view()),
-    path('api/login/', token_view.obtain_auth_token),
+    path('api/user', views.ListCreateUser.as_view()),
+    path('api/documents', views.ListCreateDocument.as_view()),
+    path('api/document/<int:id>', views.GetUpdateDeleteDocument.as_view()),
+    path('api/folder', views.ListCreateFolder.as_view()),
+    path('api/register', views.CreateUserView.as_view()),
+    path('api/login', token_view.obtain_auth_token),
 ]
